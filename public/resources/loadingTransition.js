8fb29c2df5d57c183dbcdc6dfba73582b4c208fd
isaacy13 <iyastros@gmail.com>
2020-12-26 01:32:55 -0600

    added footer w/ links 12/26/20

--- public/resources/loadingTransition.js
+++ public/resources/loadingTransition.js
@@ -1,3 +1,4 @@
+// Fades all words' opacity to 0 & loads starry sky homepage
 async function loadTransition() {
     let words = [document.getElementById("name"), document.getElementById("presenting"), document.getElementById("placeholder")];
     for (let i=0; i<words.length; i++){
@@ -7,22 +8,28 @@ async function loadTransition() {
         }
         words[i].classList.add("fadeToNone");
     }
-    await sleep(4000);
+    await sleep(3200);
     loadHomePage();
 }
 
+// loads starry sky homepage
 function loadHomePage() {
+    // get rid of previous "presentation" stuff from page
     document.getElementById("prezi").remove();
 
+    // create a div "stars" & style it "stars"
     let stars = document.createElement("div");
     stars.classList.add("stars");
 
+    // create a div "twinking" & style it "twinkling"
     let twinkling = document.createElement("div");
     twinkling.classList.add("twinkling");
 
+    // create a div "clouds" & style it "clouds"
     let clouds = document.createElement("div");
     clouds.classList.add("clouds");
-
+    
+    // create a div "title"
     let title = document.createElement("div");
     let text = document.createElement("h1");
     let textContent = document.createTextNode("HOMEPAGE");
@@ -32,8 +39,80 @@ function loadHomePage() {
 
     let body = document.getElementById("root");
     
+    // add created divs into body
     body.appendChild(stars);
     body.appendChild(twinkling);
     body.appendChild(clouds);
     body.appendChild(title);
+
+    let circle = document.createElement("span");
+    circle.classList.add("circle");
+
+    body.appendChild(circle);
+
+    // create a footer
+    let footer = document.createElement("footer");
+
+    /* add footer icon links */
+
+    // LinkedIn
+    let LinkedInLink = document.createElement("a");
+    LinkedInLink.classList.add("footerContent");
+    LinkedInLink.href = "https://www.linkedin.com/in/isaac-yeang/";
+    LinkedInLink.id = "LinkedIn-Logo";
+    
+    let LinkedInIcon = document.createElement("img");
+    LinkedInIcon.src = "resources/linkedin.png";
+    LinkedInIcon.title = "Connect with me on LinkedIn";
+    LinkedInIcon.alt = "LinkedIn Link";
+
+    LinkedInLink.appendChild(LinkedInIcon);
+    
+    // Github
+    let GithubLink = document.createElement("a");
+    GithubLink.classList.add("footerContent");
+    GithubLink.href = "https://github.com/isaacy13/";
+    GithubLink.id = "Github-Logo";
+    
+    let GithubIcon = document.createElement("img");
+    GithubIcon.src = "resources/github.png";
+    GithubIcon.title = "Check out my Github";
+    GithubIcon.alt = "Github Link";
+
+    GithubLink.appendChild(GithubIcon);
+
+    // Email
+    let EmailLink = document.createElement("a");
+    EmailLink.classList.add("footerContent");
+    EmailLink.href = "mailto:isaac.yeang@aggienetwork.com";
+    EmailLink.id = "Gmail-Logo";
+
+    let EmailIcon = document.createElement("img");
+    EmailIcon.src = "resources/gmail.png";
+    EmailIcon.title = "Email me";
+    EmailIcon.alt = "Email Link";
+
+    EmailLink.appendChild(EmailIcon);
+    
+    // append links to footer
+    footer.append(EmailLink);
+    footer.append(GithubLink);
+    footer.append(LinkedInLink);
+
+    // add footer to body
+    body.appendChild(footer);
+    
+    /*
+        <div class="stars"></div>
+        <div class="twinkling"></div>
+        <div class="clouds"></div>
+        <div style="title">
+            <h1>HOMEPAGE</h1>
+        </div>
+        <br>
+        <h1>test</h1>
+
+        <footer class="footer">
+        </footer>
+    */
 }
