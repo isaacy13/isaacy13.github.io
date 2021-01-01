5aabf4a996cdddeb4bb56aec51b7b4df83d8fed6
isaacy13 <iyastros@gmail.com>
2021-01-01 01:51:26 -0600

    added responsive cards & logo

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+// "homepage": "http://isaacy13.github.io/isaacy13.github.io",
+// "predeploy": "npm run build",
+// "deploy": "gh-pages -d build",
+
 import './App.css';
 function App() {
   return (
@@ -24,32 +28,40 @@ function App() {
           <div className="twinkling"></div>
         </div>
 
-        <div className="title">
-          <h1 style={{ textAlign: "center" }}>CONTROL CENTER</h1>
+        <div id="page-header">
+          <div id="logo-container">
+            <a href="https://isaacy13.github.io/">
+              <img src="resources/initials.png" alt="Website logo"></img>
+              <h1 id="IY-Logo">ISAAC YEANG</h1>
+            </a>
+          </div>
         </div>
 
-        <div className="navButtons">
-          <button id="leftButton" className="fadeSkipButtonIn"></button>
-          <button id="rightButton" className="fadeSkipButtonIn"></button>
-        </div>
-
-        <div id="planet-container">
-          <button id="planet">
-            <img id="planet-img" src="resources/saturn.png" alt="planet"></img>
-          </button>
+        <div className="grid grid-container">
+          <section> {/*card of information*/}
+            <p>
+              <header>ABOUT ME</header> {/*title*/}
+              <div>
+                <p>Hey! Welcome to my page. I love learning and creating! Check out some of my projects.</p>
+              </div> {/*information*/}
+            </p>
+            {/* <img src="resources/headshot.png" alt="headshot of Isaac Yeang"></img> image */}
+          </section>
+          <section></section>
+          <section></section>
         </div>
 
         <footer>
-          <a className="footerContent" href="mailto:isaac.yeang@aggienetwork.com" id="Gmail-Logo">
+          <a href="mailto:isaac.yeang@aggienetwork.com" id="Gmail-Logo">
             <img src="resources/gmail.png" title="Email me" alt="Email Link"></img>
           </a>
-          <a className="footerContent" href="https://github.com/isaacy13/" id="Github-Logo">
+          <a href="https://github.com/isaacy13/" id="Github-Logo">
             <img src="resources/github.png" title="Check out my Github" alt="Github Link"></img>
           </a>
-          <a className="footerContent" href="https://www.linkedin.com/in/isaac-yeang/" id="LinkedIn-Logo">
+          <a href="https://www.linkedin.com/in/isaac-yeang/" id="LinkedIn-Logo">
             <img src="resources/linkedin.png" title="Connect with me on LinkedIn" alt="LinkedIn Link"></img>
           </a>
-      </footer>
+        </footer>
       </div>
     </div>
   );
