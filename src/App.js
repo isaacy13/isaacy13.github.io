efe0b557bc97575dfe715d9668ebfb8b151e3540
Isaac <iyastros@gmail.com>
2021-01-11 15:23:40 -0600

    animation changes, card content changes 1/11/21

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 
     // Initial Loading Screen
     <div className="centerDiv-container">
-      <div className="centerDiv" id="prezi">
+      {/* <div className="centerDiv" id="prezi">
         <div>
           <h1 className="flyInAnimationByLetter" id="name">ISAAC YEANG</h1>
         </div>
@@ -22,9 +22,9 @@ function App() {
         <div>
           <button id="skipButton" className="fadeSkipButtonIn"></button>
         </div>
-      </div>
+      </div> */}
 
-      <div style={{ display:"none" }} id="homepage">
+      <div id="homepage">
         <div id="decorations">
           <div className="stars"></div>
           <div className="twinkling"></div>
@@ -53,93 +53,68 @@ function App() {
               </p>
             </section>
 
-            <section className="projects-card card span-6" tabindex="0">
+            <section className="projects-card card span-6" tabIndex="0">
               <header>Projects</header>
 
               <div className="card-desc">
                 <p>Study Buddy</p>
                 <p>Picture Roulette Bot</p>
                 <p>OCR Document Scanner</p>
-                <br></br>
-                <p>Attendance System</p>
-                <p>String Calculator</p>
-                <br></br>
-                <p>Eagle Scout Project</p>
                 <p>...</p>
-                <br></br>
               </div>
             </section>
 
-            <section className="skills-interests-card card span-6" tabindex="0">
+            <section className="skills-interests-card card span-6" tabIndex="0">
               <header>Skills & Interests</header>
 
               <div className="card-desc">
                   <p>C/C++</p>
                   <p>Python</p>
                   <p>Java</p>
-                  <p>Android Studio</p>
-                  <br></br>
-                  <p>Strong Work Ethic</p>
-                  <p>Demonstrated Leadership</p>
-                  <p>Adaptability</p>
                   <p>...</p>
               </div>
             </section>
 
-            <section className="education-card card span-6" tabindex="0">
+            <section className="education-card card span-6" tabIndex="0">
               <header>Education & Work Experience</header>
 
               <div className="card-desc">
                 <p>Texas A&M University College Station</p>
-                <br></br>
                 <p>Texas A&M University Corpus Christi</p>
                 <p>Del Mar College</p>
-                <p>Flour Bluff High School</p>
-                <br></br>
-                <p>Eyeland Vision</p>
-                <p>Corpus Christi Optometric</p>
                 <p>...</p>
               </div>
             </section>
       
-            <section className="achievements-awards-card card span-6" tabindex="0">
+            <section className="achievements-awards-card card span-6" tabIndex="0">
               <header>Awards</header>
 
               <div className="card-desc">
                 <p>Texas A&M Engineering Honors</p>
                 <p>Texas A&M Dean's Honor Roll</p>
                 <p>Texas A&M CC Anchor Academic Excellence List</p>
-                <p>Texas A&M CC Dean's List</p>
-                <br></br>
-                <p>Eagle Scout</p>
-                <br></br>
-                <p>First Year Research Conference: Quantum Computing</p>
                 <p>...</p>
               </div>
             </section>
 
-            <section className="affiliations-card card span-6" tabindex="0">
+            <section className="affiliations-card card span-6" tabIndex="0">
               <header>Affiliations</header>
 
               <div className="card-desc">
                 <p>Aggie Coding Club</p>
                 <p>TAMU Howdy Hack</p>
                 <p>TAMU Datathon</p>
-                <br></br>
-                <p>Boy Scouts of America</p>
                 <p>...</p>
               </div>
             </section>
 
-            <section className="goals-card card span-6" tabindex="0">
+            <section className="goals-card card span-6" tabIndex="0">
               <header>Ambitions & Goals</header>
 
               <div className="card-desc">
                 <p>Summer 2020 Internship</p>
                 <p>Summer School 2020</p>
                 <p>Graduate Fall 2022</p>
-                <br></br>
-                <p>Land a passionate job</p>
                 <p>...</p>
               </div>
             </section>    
@@ -153,12 +128,74 @@ function App() {
               <h2 id="popup-title">Projects Overview</h2>
             </div>
             <div className="popup-window-body">
-              <div className="projects-popup popup-text">Projects Text</div>
-              <div className="skills-interests-popup popup-text">Skills / Interests Text</div>
-              <div className="education-popup popup-text">Education Text</div>
-              <div className="achievements-awards-popup popup-text">Achievements / Awards Text</div>
-              <div className="affiliations-popup popup-text">Affiliations Text</div>
-              <div className="goals-popup popup-text">Goals Text</div>
+              <div className="projects-popup popup-text">
+                <div className="card-desc">
+                  <p>Study Buddy</p>
+                  <p>Picture Roulette Bot</p>
+                  <p>OCR Document Scanner</p>
+                  <br></br>
+                  <p>Attendance System</p>
+                  <p>String Calculator</p>
+                  <br></br>
+                  <p>Eagle Scout Project</p>
+                  <br></br>
+                </div>
+              </div>
+              <div className="skills-interests-popup popup-text">
+                <div className="card-desc">
+                    <p>C/C++</p>
+                    <p>Python</p>
+                    <p>Java</p>
+                    <p>Android Studio</p>
+                    <br></br>
+                    <p>Strong Work Ethic</p>
+                    <p>Demonstrated Leadership</p>
+                    <p>Adaptability</p>
+                </div>
+              </div>
+              <div className="education-popup popup-text">
+                <div className="card-desc">
+                  <p>Texas A&M University College Station</p>
+                  <br></br>
+                  <p>Texas A&M University Corpus Christi</p>
+                  <p>Del Mar College</p>
+                  <p>Flour Bluff High School</p>
+                  <br></br>
+                  <p>Eyeland Vision</p>
+                  <p>Corpus Christi Optometric</p>
+                </div>
+              </div>
+              <div className="achievements-awards-popup popup-text">
+                <div className="card-desc">
+                  <p>Texas A&M Engineering Honors</p>
+                  <p>Texas A&M Dean's Honor Roll</p>
+                  <p>Texas A&M CC Anchor Academic Excellence List</p>
+                  <p>Texas A&M CC Dean's List</p>
+                  <br></br>
+                  <p>Eagle Scout</p>
+                  <br></br>
+                  <p>First Year Research Conference: Quantum Computing</p>
+                </div>
+              </div>
+              <div className="affiliations-popup popup-text">
+                <div className="card-desc">
+                  <p>Aggie Coding Club</p>
+                  <p>TAMU Howdy Hack</p>
+                  <p>TAMU Datathon</p>
+                  <br></br>
+                  <p>Boy Scouts of America</p>
+                </div>
+              </div>
+              <div className="goals-popup popup-text">
+                <div className="card-desc">
+                  <p>Summer 2020 Internship</p>
+                  <p>Summer School 2020</p>
+                  <p>Graduate Fall 2022</p>
+                  <br></br>
+                  <p>Land a passionate job</p>
+                  <p>...</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
