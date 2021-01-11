b69ac1e05edc99ba7449c42924a7d97f48828809
Isaac <iyastros@gmail.com>
2021-01-11 15:25:10 -0600

    Updates

--- resources/homepage.js
+++ resources/homepage.js
@@ -29,3 +29,33 @@ function closePopup(num) {
     cardsDesc[num].style.display = "none";
     document.querySelector(".popup-window").style.display = "none";
 }
+
+// auto scroll to top on refresh
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+}
+
+// animate card fly in
+let cardsAnimated = [];
+for (let i = 0; i < cards.length; i++)
+    cardsAnimated.push(false);
+
+for (let i = 0; i < cards.length; i++) {
+    window.addEventListener("scroll", function() {
+    if (isInViewport(cards[i]) && !cardsAnimated[i]) {
+        cardsAnimated[i] = true;
+        cards[i].classList.add("cardFlyIn");
+    }
+    });
+}
+
+function isInViewport(element) {
+    var rect = element.getBoundingClientRect();
+    var html = document.documentElement;
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || html.clientHeight) &&
+      rect.right <= (window.innerWidth || html.clientWidth)
+    );
+}
