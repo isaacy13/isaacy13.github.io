4e523882f935b209cd56c94bee10c78f59617b97
Isaac <iyastros@gmail.com>
2021-01-15 21:49:12 -0600

    Updates

--- resources/awards.js
+++ resources/awards.js
@@ -1,6 +1,13 @@
+// height setter for large picture
 updateHeight();
 window.addEventListener("resize", updateHeight);
 
 function updateHeight() {
     document.querySelector(".welcome-container").style.height = window.innerHeight + "px";
 }
+
+// card animations on scroll in view
+let awardsCards = document.getElementsByTagName("td");
+let awardsCardsAnimated = []
+for (let i = 0; i < awardsCards.length; i++)
+    awardsCardsAnimated.push(false);
