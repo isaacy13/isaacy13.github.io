87f1053d8f32e0d1c8016907bd5335197c8d3e75
Isaac <iyastros@gmail.com>
2021-01-15 22:19:49 -0600

    hotfix: broken image on iphone 1/15/21

--- public/resources/awards.js
+++ public/resources/awards.js
@@ -5,9 +5,3 @@ window.addEventListener("resize", updateHeight);
 function updateHeight() {
     document.querySelector(".welcome-container").style.height = window.innerHeight + "px";
 }
-
-// card animations on scroll in view
-let awardsCards = document.getElementsByTagName("td");
-let awardsCardsAnimated = []
-for (let i = 0; i < awardsCards.length; i++)
-    awardsCardsAnimated.push(false);
