52b4461926c7c8409d2acd4b2be4d879ff7210a3
Isaac <iyastros@gmail.com>
2021-01-27 23:17:15 -0600

    Updates

--- resources/affiliations.js
+++ resources/affiliations.js
@@ -5,7 +5,9 @@ let affil_cards_info = [".hh-info", ".acc-info", ".datathon-info", ".honors-info
 
 for (let i = 0; i < affil_cards.length; i++) {
     document.addEventListener("mousemove", function() {
-        var c = window.getComputedStyle(document.querySelector(affil_cards[i])).getPropertyValue("border-top-style");
+        try {
+            var c = window.getComputedStyle(document.querySelector(affil_cards[i])).getPropertyValue("border-top-style");
+        } catch { return; }
 
         if (c == "hidden") {
             document.querySelector(affil_cards_hover[i]).style.display = "block";
@@ -15,4 +17,37 @@ for (let i = 0; i < affil_cards.length; i++) {
             document.querySelector(affil_cards_info[i]).style.display = "block";
         }
     }, false);
+}
+
+let tmp = document.querySelectorAll(".affil-card");
+
+document.addEventListener("scroll", function() {
+    const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+
+    if (vw < 550) {
+        for (let i = 0; i < tmp.length; i++) {
+            if (isElementInViewport(tmp[i])) { 
+                tmp[i].style = "opacity: 1;";
+            } else {
+                tmp[i].style = "";
+            }
+        }
+    }
+}, false);
+
+function isElementInViewport(el) {
+
+    // Special bonus for those using jQuery
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+        el = el[0];
+    }
+
+    var rect = el.getBoundingClientRect();
+
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /* or $(window).height() */
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
+    );
 }
